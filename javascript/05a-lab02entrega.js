8065e05cd768aba7ef5b1f67050fdf3323e9c9f2
Horacio Burgos <horaciomardel@hotmail.com>
2023-07-31 16:05:46 -0300

    entrega de lab02

---
+++ javascript/05a-lab02entrega.js
@@ -0,0 +1,22 @@
+const objectPerson0 = {
+    firstName:"PEDRO",
+    lastName : "RAMIREZ",
+    birthday:20,
+    status:false,
+}
+const objectPerson1 = {
+    firstName:"juan",
+    lastName : "RAMI",
+    birthday:28,
+    status:true,
+}
+console.log(objectPerson0);
+console.log(objectPerson0.firstName);
+console.log(objectPerson0.lastName);
+console.log(objectPerson0.birthday);
+console.log(objectPerson0.status);
+
+
+console.log("*************array de objeto***********");
+const arrayObjeto = [objectPerson0,objectPerson0];
+console.log(arrayObjeto);
